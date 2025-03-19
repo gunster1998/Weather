a755034fe5f2ec57f45668bdda296ae9026d810c
Konstantin Dryagin <gunster98@gmail.com>
2025-03-19 11:44:46 +0300

    feat: Добавил спинер загрузки

--- global.js
+++ global.js
@@ -16,7 +16,6 @@ async function getGeolocation() {
 }
 //Адресс по координатам
 async function getAdress({ latitude, longitude }) {
-
   const response = await fetch(
     `https://geocode-maps.yandex.ru/1.x/?apikey=${APIYANDEX}&geocode=${longitude},${latitude}&format=json`
   );
@@ -30,68 +29,70 @@ async function getAdress({ latitude, longitude }) {
     throw new Error("Адрес не найден в ответе API");
   }
 
-  const formatedAdres = descriptionAdress.split(',');
-  
+  const formatedAdres = descriptionAdress.split(",");
+
   return formatedAdres[0];
 }
 
 //Погода по координатам
 async function getWether(location) {
-    const loc =
-      typeof location === "string"
-        ? location
-        : `${location.latitude},${location.longitude}`;
+  const loc =
+    typeof location === "string"
+      ? location
+      : `${location.latitude},${location.longitude}`;
 
-    const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}/?key=${APIWEATHER}&unitGroup=metric&lang=ru`
-    );
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}/?key=${APIWEATHER}&unitGroup=metric&lang=ru`
+  );
 
-    if (!response.ok) {
-      throw new Error('Ошибка в HTTP getWeather: ',response.status)
-    }
+  if (!response.ok) {
+    throw new Error("Ошибка в HTTP getWeather: ", response.status);
+  }
 
-    const responseJSON = await response.json();
+  const responseJSON = await response.json();
 
+  const descriptionDay = responseJSON.days[0].conditions;
+  const temp = responseJSON.currentConditions.temp;
+  const feelslikeDay = responseJSON.currentConditions.feelslike;
+  const humidity = responseJSON.currentConditions.humidity;
+  const cloudcover = responseJSON.currentConditions.cloudcover;
+  const windspeed = responseJSON.currentConditions.windspeed;
 
-    const descriptionDay = responseJSON.days[0].conditions;
-    const temp = responseJSON.currentConditions.temp;
-    const feelslikeDay = responseJSON.currentConditions.feelslike;
-    const humidity = responseJSON.currentConditions.humidity;
-    const cloudcover = responseJSON.currentConditions.cloudcover;
-    const windspeed = responseJSON.currentConditions.windspeed;
-
-    console.log(responseJSON);
-    console.log(`Температура ${temp}° градусов`);
-    console.log("Температура ощущается: ", feelslikeDay);
-    console.log(`Состояние дня: `, descriptionDay);
-    console.log("Влажность: ", humidity);
-    console.log("Облачность: ", cloudcover);
-    console.log("Скорость ветра: ", windspeed);
-    return {
-      responseJSON,
-      temp,
-      descriptionDay,
-      feelslikeDay,
-      humidity,
-      cloudcover,
-      windspeed,
-    };
+  console.log(responseJSON);
+  console.log(`Температура ${temp}° градусов`);
+  console.log("Температура ощущается: ", feelslikeDay);
+  console.log(`Состояние дня: `, descriptionDay);
+  console.log("Влажность: ", humidity);
+  console.log("Облачность: ", cloudcover);
+  console.log("Скорость ветра: ", windspeed);
+  return {
+    responseJSON,
+    temp,
+    descriptionDay,
+    feelslikeDay,
+    humidity,
+    cloudcover,
+    windspeed,
+  };
 }
 
-function rendertCitySection ({
-  responseJSON,
-  temp,
-  descriptionDay,
-  feelslikeDay,
-  humidity,
-  cloudcover,
-  windspeed,
-},formatedAdres) {
-  const nameCity = document.querySelector('.weatherCitySection__city')
-  const tempCity = document.querySelector('.weatherCitySection__degrees')
-  const timeCite = document.querySelector('.weatherCitySection__time')
+function rendertCitySection(
+  {
+    responseJSON,
+    temp,
+    descriptionDay,
+    feelslikeDay,
+    humidity,
+    cloudcover,
+    windspeed,
+  },
+  formatedAdres
+) {
+  const nameCity = document.querySelector(".weatherCitySection__city");
+  const tempCity = document.querySelector(".weatherCitySection__degrees");
+  const timeCite = document.querySelector(".weatherCitySection__time");
 
-console.log('')
+  console.log("");
   const dateNow = new Date();
   const daysWeek = [
     "Воскресенье",
@@ -100,7 +101,7 @@ console.log('')
     "Среда",
     "Четверг",
     "Пятница",
-    "Суббота"
+    "Суббота",
   ];
   const ArrayMonths = [
     "Января",
@@ -114,26 +115,26 @@ console.log('')
     "Сентября",
     "Октября",
     "Ноября",
-    "Декабря"
+    "Декабря",
   ];
   const hourse = dateNow.getHours();
-  const minutes = ()=> {
-    if (dateNow.getMinutes()> 9) {
+  const minutes = () => {
+    if (dateNow.getMinutes() > 9) {
       return dateNow.getMinutes().toString();
     }
-    return ("0" + dateNow.getMinutes())
-  }
+    return "0" + dateNow.getMinutes();
+  };
   const day = daysWeek[dateNow.getDay()];
   const dayMonth = dateNow.getDate();
   const month = ArrayMonths[dateNow.getMonth()];
   const year = dateNow.getFullYear();
 
-  nameCity.innerHTML = `${formatedAdres}`
-  tempCity.innerHTML = `${Math.floor(temp)}°`
-  timeCite.innerHTML = `${hourse}:${minutes()} - ${day}, ${dayMonth} ${month} ‘${year}`
+  nameCity.innerHTML = `${formatedAdres}`;
+  tempCity.innerHTML = `${Math.floor(temp)}°`;
+  timeCite.innerHTML = `${hourse}:${minutes()} - ${day}, ${dayMonth} ${month} ‘${year}`;
 }
 
-function renderMoreDetailed ({
+function renderMoreDetailed({
   responseJSON,
   temp,
   descriptionDay,
@@ -142,56 +143,65 @@ function renderMoreDetailed ({
   cloudcover,
   windspeed,
 }) {
-  const moreDetailed = document.querySelector('.moreDetailed__description')
-  const tempNow = document.querySelector('.tempNow__meaning')
-  const tempFeels = document.querySelector('.tempFeels__meaning')
-  const humidityBlock = document.querySelector('.humidity__meaning')
-  const cloudcoverBlock = document.querySelector('.cloudcover__meaning')
-  const windSpeedBlock = document.querySelector('.windspeed__meaning')
+  const moreDetailed = document.querySelector(".moreDetailed__description");
+  const tempNow = document.querySelector(".tempNow__meaning");
+  const tempFeels = document.querySelector(".tempFeels__meaning");
+  const humidityBlock = document.querySelector(".humidity__meaning");
+  const cloudcoverBlock = document.querySelector(".cloudcover__meaning");
+  const windSpeedBlock = document.querySelector(".windspeed__meaning");
 
-  moreDetailed.innerHTML = `${descriptionDay}`
-  tempNow.innerHTML = `${temp}°`
-  tempFeels.innerHTML = `${feelslikeDay}°`
-  humidityBlock.innerHTML = `${humidity}%`
-  cloudcoverBlock.innerHTML = `${cloudcover}%`
-  windSpeedBlock.innerHTML = `${windspeed}%`
+  moreDetailed.innerHTML = `${descriptionDay}`;
+  tempNow.innerHTML = `${temp}°`;
+  tempFeels.innerHTML = `${feelslikeDay}°`;
+  humidityBlock.innerHTML = `${humidity}%`;
+  cloudcoverBlock.innerHTML = `${cloudcover}%`;
+  windSpeedBlock.innerHTML = `${windspeed}%`;
 }
-
 
 function addCiteWeatherListener() {
-  const input = document.querySelector('.search-form__input')
-  const form = document.querySelector('.search-form')
+  const input = document.querySelector(".search-form__input");
+  const form = document.querySelector(".search-form");
 
-  input.addEventListener('change',async function (event) {
-    const city = event.target.value
-    console.log("Вот адресс тут для евента",city)
+  input.addEventListener("change", async function (event) {
+    const city = event.target.value;
+    console.log("Вот адресс тут для евента", city);
     const weatherCity = await getWether(city);
-    console.log(weatherCity)
-    rendertCitySection(weatherCity,city);
-    renderMoreDetailed(weatherCity)
-  })
+    console.log(weatherCity);
+    rendertCitySection(weatherCity, city);
+    renderMoreDetailed(weatherCity);
+  });
 
-  form.addEventListener('submit',function(event) {
+  form.addEventListener("submit", function (event) {
     event.preventDefault();
-
-  })
+  });
 }
 
+function addSpinner(action) {
+  const spinnerBlock = document.querySelector(".spinner__block");
+  const mainBlock = document.querySelector(".oneScreen");
 
-
-
-
+  if (action === "on") {
+    mainBlock.classList.add("hidden");
+    spinnerBlock.classList.remove("hidden");
+  } else if (action === "off") {
+    mainBlock.classList.remove("hidden");
+    spinnerBlock.classList.add("hidden");
+  }
+}
 
 (async () => {
   try {
+    addSpinner("on");
     const coordinates = await getGeolocation();
     const weather = await getWether(coordinates);
     const adress = await getAdress(coordinates);
-    rendertCitySection(weather,adress);
-    renderMoreDetailed(weather)
-    addCiteWeatherListener() 
+    rendertCitySection(weather, adress);
+    renderMoreDetailed(weather);
+    addCiteWeatherListener();
+    addSpinner("off");
   } catch (error) {
     if (error.code === 1) {
+      addSpinner("off");
       console.log("Пользователь отклонил запрос на геолокацию.");
     } else {
       console.error("Ошибка: ", error.message);
